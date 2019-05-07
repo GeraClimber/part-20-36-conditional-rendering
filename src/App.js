452739f815126b880f9b012bbdb8c1a78db511f7
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2019-05-07 21:22:49 +0300

    add conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {Component} from "react"
+import Conditional from "./Conditional"
 
-
-function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoading: true
+        }
+    }
+    
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 1500)
+    }
+    
+    render() {
+        return (
+            <div>
+                {this.state.isLoading ?
+                <h1>Loading...</h1> :
+                <Conditional />}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
